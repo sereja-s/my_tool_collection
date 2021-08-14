6f6d43dade507ed7712ecaeb15bc078d3e1d3052
sereja-s <serejasuvorov1979@gmail.com>
2021-08-15 00:01:04 +0300

    spoiler  (продолжение)

--- Spoiler/spoiler.js
+++ Spoiler/spoiler.js
@@ -33,8 +33,50 @@ if (spoilersArray.length > 0) {
 			const breakpoint = {};
 			/* преобразовываем строку внутри params в массив (с помощью split) с разделителем (запятая) и сохраняем в константе: */
 			const paramsArray = params.split(",");
-			/*  для пустого объкта breakpoint создаём значение value и присваиваем туда нулевую ячейку массива: */
+			/*  для пустого объкта breakpoint создаём значение value и присваиваем туда нулевую ячейку массива (ширина экрана): */
 			breakpoint.value = paramsArray[0];
+			/* соэраним значение min для этого объекта ( по умолчанию будет-max) */
+			breakpoint.type = paramsArray[1] ? paramsArray[1].trim() : "max";
+			/* сохраним сам объект */
+			breakpoint.item = item;
+			/* заполненный значениями объект breakpoint добавляем в массив breakpointsArray: */
+			breakpointsArray.push(breakpoint);
+		});
+
+		// Что бы учесть повторения, получаем уникальные брейкпоинты:
+		/* сохраним в переменной, переделанный с помощью метода map массив breakpointsArray*/
+		let mediaQueries = breakpointsArray.map(function (item) {
+			/* соберём строку с медиа-запросом: */
+			return '(' + item.type + "-width:" + item.value + "px)," + item.value + ',' + item.type;
+		});
+		/* через переменную mediaQueries(массив), обращаемся к функции и фильтруем массив и возвращаем уникальные значения и сохраняем внутри массива mediaQueries*/
+		mediaQueries = mediaQueries.filter(function (item, index, self) {
+			return self.indexOf(item) === index;
+		});
+
+		// Работаем с каждым брейкпоинтом
+		/* пробежимся по массиву mediaQueries, который мы собрали.убрали все дубли и т.д. ,с помощью forEach ( по всем брейкпоинтам */
+		mediaQueries.forEach(breakpoint => {
+			/* в константу получаем строку(одну запись данного массива), которую преобразуем в массив с помощью разделителя (запятая) */
+			const paramsArray = breakpoint.split(",");
+			/*  в константу получаем первый параметр массива параметров (число-ширина экрана) */
+			const mediaBreakpoint = paramsArray[1];
+			/* в константу получаем второй параметр массива параметров (число-ширина экрана) */
+			const mediaType = paramsArray[2];
+			/* в константу сохраним результат работы метода matchMedia, который будет слушать ширину жкрана и отрабатывать если сработает тот или иной брейк поинт В параметры(в скобках) указываем нулевой параметр массива (строка, которую мы форировали ранее)*/
+			const matchMedia = window.matchMedia(paramsArray[0]);
+			// Соберём массив обектов, которые соответствуют данному брейкпоинту, фильтруем и сохраним в константу
+			const spoilersArray = breakpointsArray.filter(function (item) {
+				/* если совпадает и число и тип, то объект попадает в массив spoilersArray */
+				if (item.value === mediaBreakpoint && item.type === mediaType) {
+					return true;
+				}
+			});
+			// Создадим событие,которое будет отрабатывать при достижении условия брейкпоинта
+			matchMedia.addEventListener(function () {
+				initSpoilers(spoilersArray, matchMedia);
+			});
+			initSpoilers(spoilersArray, matchMedia);
 		});
 	}
 }
